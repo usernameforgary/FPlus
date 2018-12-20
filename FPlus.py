0c584345c8505e23af7d709a4cc50a4f6bd96c9e
usernameforgary <usernameforgary@gmail.com>
2018-12-21 01:41:19 +0800

    ignore FPlus.py file

--- FPlus.py
+++ FPlus.py
@@ -9,43 +9,8 @@
 
 import wx
 import wx.xrc
-import wx.dataview
 import wx.aui
-
-###########################################################################
-## Class projectViewGUI
-###########################################################################
-
-class projectViewGUI ( wx.Frame ):
-
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Project", pos = wx.DefaultPosition, size = wx.Size( 1369,824 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-
-		mainSizer = wx.BoxSizer( wx.VERTICAL )
-
-		leftSizer = wx.BoxSizer( wx.HORIZONTAL )
-
-		self.projectTreeCtrl = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TR_DEFAULT_STYLE )
-		leftSizer.Add( self.projectTreeCtrl, 1, wx.ALL|wx.EXPAND, 5 )
-
-		self.m_dataViewTreeCtrl1 = wx.dataview.DataViewTreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
-		leftSizer.Add( self.m_dataViewTreeCtrl1, 1, wx.ALL|wx.EXPAND, 5 )
-
-
-		mainSizer.Add( leftSizer, 1, wx.EXPAND, 0 )
-
-
-		self.SetSizer( mainSizer )
-		self.Layout()
-		self.projectViewStatusBar = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
-
-		self.Centre( wx.BOTH )
-
-	def __del__( self ):
-		pass
-
+import wx.dataview
 
 ###########################################################################
 ## Class AppGUI
@@ -104,5 +69,49 @@ class AppGUI ( wx.Frame ):
 	# Virtual event handlers, overide them in your derived class
 	def viewProject( self, event ):
 		event.Skip()
+
+
+###########################################################################
+## Class projectViewGUI
+###########################################################################
+
+class projectViewGUI ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Project", pos = wx.DefaultPosition, size = wx.Size( 1369,824 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		self.viewProjectMenubar = wx.MenuBar( 0 )
+		self.addMenu = wx.Menu()
+		self.addMenuItem = wx.MenuItem( self.addMenu, wx.ID_ANY, u"Add Product", wx.EmptyString, wx.ITEM_NORMAL )
+		self.addMenu.Append( self.addMenuItem )
+
+		self.viewProjectMenubar.Append( self.addMenu, u"Add" )
+
+		self.SetMenuBar( self.viewProjectMenubar )
+
+		mainSizer = wx.BoxSizer( wx.VERTICAL )
+
+		leftSizer = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.projectTreeCtrl = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TR_DEFAULT_STYLE )
+		leftSizer.Add( self.projectTreeCtrl, 1, wx.ALL|wx.EXPAND, 5 )
+
+		self.m_dataViewTreeCtrl1 = wx.dataview.DataViewTreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+		leftSizer.Add( self.m_dataViewTreeCtrl1, 1, wx.ALL|wx.EXPAND, 5 )
+
+
+		mainSizer.Add( leftSizer, 1, wx.EXPAND, 0 )
+
+
+		self.SetSizer( mainSizer )
+		self.Layout()
+		self.projectViewStatusBar = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
 
 
